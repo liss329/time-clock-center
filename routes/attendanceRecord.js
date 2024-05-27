76e9a7b38759fd5ca266e1dee22d96966abef037
Lissana <liss.glace471@gmail.com>
2024-05-28 01:49:23 +0900

    feat: 勤怠管理画面の枠組みの実装
    ・クエリパラメータで表示させる年月を変更させる
    ・クエリパラメータがなければ現在月を表示させる

--- routes/attendanceRecord.js
+++ routes/attendanceRecord.js
@@ -3,7 +3,25 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   if (!req.session.login) return res.redirect("/login");
-  res.render("attendanceRecord", { title: "勤怠管理 | Time Clock Center" });
+
+  // 今月が何日まであるか
+  const getLastDay = (year, month) => {
+    return new Date(year, month, 0).getDate();
+  };
+
+  const year = req.query.month
+    ? req.query.month.split("-")[0]
+    : new Date().getFullYear();
+  const month = req.query.month
+    ? req.query.month.split("-")[1]
+    : new Date().getMonth() + 1;
+
+  res.render("attendanceRecord", {
+    title: "勤怠管理 | Time Clock Center",
+    year: Number(year),
+    month: Number(month),
+    lastDay: getLastDay(year, month),
+  });
 });
 
 module.exports = router;
